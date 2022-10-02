fc2ede256e858a6b1484a1d8a49f3e3d5a7268d2
Bartholomé Gili <bartho.gili@gmail.com>
2022-10-02 20:19:57 +0200

    refactor(commands/plugin): beautify `info`
    Co-authored-by: Artemus <Mr-Artemus@users.noreply.github.com>

--- src/commands/plugin/info.ts
+++ src/commands/plugin/info.ts
@@ -1,5 +1,7 @@
 import { getPluginFromMonorepo, logger } from "@utils"
+import chalk from "chalk"
 import { createCommand } from "commander"
+import boxen from "boxen"
 
 export default createCommand()
 
@@ -14,7 +16,25 @@ export default createCommand()
         
         const plugin = await getPluginFromMonorepo(query)
 
-        if (plugin) logger.log(JSON.stringify(plugin))
+        if (plugin) {
+
+            const message = boxen(
+                `${chalk.bold('id:')} ${chalk.italic(query)}\n` +
+                `${chalk.bold('description:')} ${plugin.description}\n` +
+                `${chalk.bold('author:')} ${plugin.author}\n` +
+                `${chalk.bold('version:')} ${plugin.version}\n` +
+                `${chalk.bold('tscord required version:')} ${plugin.tscordRequiredVersion}`,
+                {
+                    title: chalk.bold.magentaBright(plugin.name),
+                    titleAlignment: 'center',
+                    padding: 1,
+                    borderStyle: 'round',
+                    dimBorder: true
+                }
+            )
+
+            logger.log(message)
+        }
         else logger.failure('Plugin not found.')
         
         logger.newLine()
---
+++ src/utils/string.ts
@@ -0,0 +1,4 @@
+export const multiLine = (strings: string) => {
+
+    return strings.replace(/^ +/gm, '')
+}
